1f0e855bc199bd042b8b4a0541e0428b0974859e
DX204 <dx204@piv-wifi-18-89.pivotallabs.com>
2015-02-17 15:14:49 -0500

    JonathanY - removed asserts

--- KZPropertyMapper/KZPropertyMapperCommon.h
+++ KZPropertyMapper/KZPropertyMapperCommon.h
@@ -10,17 +10,12 @@ typedef NS_ENUM(NSInteger, kErrorCode) {
 
 extern NSError *pixle_NSErrorMake(NSString *message, NSUInteger code, NSDictionary *aUserInfo, SEL selector);
 
-#define AssertTrueOrReturnNoBlock(condition, block) do{ NSAssert((condition), @"Invalid condition not satisfying: %s", #condition);\
-if(!(condition)) { block(pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd)); return NO;} }while(0)
+#define AssertTrueOrReturnNoBlock(condition, block) do{ if(!(condition)) { block(pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd)); return NO;} }while(0)
 
-#define AssertTrueOrReturnNo(condition) do{ NSAssert((condition), @"Invalid condition not satisfying: %s", #condition);\
-if(!(condition)) { pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd); return NO;} } while(0)
+#define AssertTrueOrReturnNo(condition) do{ if(!(condition)) { pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd); return NO;} } while(0)
 
-#define AssertTrueOrReturn(condition) do{ NSAssert((condition), @"Invalid condition not satisfying: %s", #condition);\
-if(!(condition)) { pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd); return;} } while(0)
+#define AssertTrueOrReturn(condition) do{ if(!(condition)) { pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd); return;} } while(0)
 
-#define AssertTrueOrReturnNil(condition) do{ NSAssert((condition), @"Invalid condition not satisfying: %s", #condition);\
-if(!(condition)) { pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd); return nil;}} while(0)
+#define AssertTrueOrReturnNil(condition) do{ if(!(condition)) { pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd); return nil;}} while(0)
 
-#define AssertTrueOrReturnErrors(condition) do{ NSAssert((condition), @"Invalid condition not satisfying: %s", #condition);\
-if(!(condition)) { return @[pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd)];} }while(0)
+#define AssertTrueOrReturnErrors(condition) do{ if(!(condition)) { return @[pixle_NSErrorMake([NSString stringWithFormat:@"Invalid condition not satisfying: %s", #condition], kErrorCodeInternal, nil, _cmd)];} }while(0)
